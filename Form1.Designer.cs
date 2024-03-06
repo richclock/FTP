5a146a9bbd043bd10b71b3502335751329f50af1
Clark Liao <richclock@yahoo.com.tw>
2024-03-06 13:16:30 +0800

    test ftp server

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace FTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetFileList = new System.Windows.Forms.Button();
+            this.btnDeleteFile = new System.Windows.Forms.Button();
+            this.btnUploadFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetFileList
+            // 
+            this.btnGetFileList.Location = new System.Drawing.Point(287, 110);
+            this.btnGetFileList.Name = "btnGetFileList";
+            this.btnGetFileList.Size = new System.Drawing.Size(130, 46);
+            this.btnGetFileList.TabIndex = 0;
+            this.btnGetFileList.Text = "取得檔案列表";
+            this.btnGetFileList.UseVisualStyleBackColor = true;
+            this.btnGetFileList.Click += new System.EventHandler(this.btnGetFileList_Click);
+            // 
+            // btnDeleteFile
+            // 
+            this.btnDeleteFile.Location = new System.Drawing.Point(335, 202);
+            this.btnDeleteFile.Name = "btnDeleteFile";
+            this.btnDeleteFile.Size = new System.Drawing.Size(130, 46);
+            this.btnDeleteFile.TabIndex = 1;
+            this.btnDeleteFile.Text = "刪除檔案";
+            this.btnDeleteFile.UseVisualStyleBackColor = true;
+            this.btnDeleteFile.Click += new System.EventHandler(this.btnDeleteFile_Click);
+            // 
+            // btnUploadFile
+            // 
+            this.btnUploadFile.Location = new System.Drawing.Point(469, 292);
+            this.btnUploadFile.Name = "btnUploadFile";
+            this.btnUploadFile.Size = new System.Drawing.Size(130, 46);
+            this.btnUploadFile.TabIndex = 2;
+            this.btnUploadFile.Text = "上傳檔案";
+            this.btnUploadFile.UseVisualStyleBackColor = true;
+            this.btnUploadFile.Click += new System.EventHandler(this.btnUploadFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ip:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(84, 25);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 22);
+            this.txtIP.TabIndex = 4;
+            this.txtIP.Text = "192.168.0.153";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(84, 53);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 22);
+            this.txtUsername.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "username:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(84, 81);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "password:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUploadFile);
+            this.Controls.Add(this.btnDeleteFile);
+            this.Controls.Add(this.btnGetFileList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetFileList;
+        private System.Windows.Forms.Button btnDeleteFile;
+        private System.Windows.Forms.Button btnUploadFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace FTP
+{
+    public partial class Form1 : Form
+    {
+        FtpWebRequest _request;
+        string _username;
+        string _password;
+        string _ftpServer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetFileList_Click(object sender, EventArgs e)
+        {
+            string path = "";
+            var ncList = GetProgramList(path);
+        }
+
+        private void btnDeleteFile_Click(object sender, EventArgs e)
+        {
+            string path = "";
+            var ncList = GetProgramList(path);
+            if (ncList.Count > 0)
+            {
+                try
+                {
+                    foreach (var prog in ncList)
+                    {
+                        string deletePath = "ftp://" + _ftpServer + "//" + prog;
+                        _request = (FtpWebRequest)WebRequest.Create(deletePath);
+                        _request.Credentials = new NetworkCredential(_username, _password);
+                        _request.UseBinary = true;
+                        _request.KeepAlive = false;
+                        _request.Method = WebRequestMethods.Ftp.DeleteFile;
+                        var response = (FtpWebResponse)_request.GetResponse();
+                        response.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("刪除檔案出錯：" + ex.Message);
+                }
+            }
+        }
+        private void btnUploadFile_Click(object sender, EventArgs e)
+        {
+            string filePath = "";
+            UploadProgram(filePath);
+        }
+        List<string> GetProgramList(string path)
+        {
+            _ftpServer = txtIP.Text;
+            List<string> ncList = new List<string>();
+            string filePath = "ftp://" + _ftpServer + "//";
+            _request = (FtpWebRequest)WebRequest.Create(filePath);
+            _request.Credentials = new NetworkCredential(_username, _password);
+            _request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            using (WebResponse response = _request.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        if (!line.Contains("<DIR>"))
+                        {
+                            string msg = line.Substring(39).Trim();
+                            ncList.Add(msg);
+                        }
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            return ncList;
+        }
+        void UploadProgram(string filePath)
+        {
+            string progName = Path.GetFileName(filePath);
+            string path = "ftp://" + _ftpServer + "//" + progName;
+            _request = (FtpWebRequest)WebRequest.Create(path);
+            _request.Credentials = new NetworkCredential(_username, _password);
+            _request.Method = WebRequestMethods.Ftp.UploadFile;
+            StreamReader sourceStream = new StreamReader(filePath);
+            byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            _request.ContentLength = fileContents.Length;
+            Stream requestStream = _request.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+            requestStream.Close();
+            using (var response = (FtpWebResponse)_request.GetResponse())
+            {
+                response.Close();
+            }
+        }
+
+        
+    }
+}
